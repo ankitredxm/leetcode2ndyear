d283e148acd6356c2bfe5bac855716893f5ab41d
Ankit Kumar <167794543+ankitredxm@users.noreply.github.com>
2025-05-11 16:24:14 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (48.28%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int lo=0;
+        int hi=arr.size()-1;
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if(arr[mid]>arr[mid+1]) hi=mid;
+            else{
+                lo=mid+1;
+            }
+        }
+        return lo;
+    }
+};
