cc7b11be4dc68c242f7f470a738755730a1d73de
Ankit Kumar <167794543+ankitredxm@users.noreply.github.com>
2025-05-11 08:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (17.28%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,42 +3,41 @@ public:
     vector<int> searchRange(vector<int>& nums, int t) {
         int lo=0;
         int hi=nums.size()-1;
-        int f=-1,l=-1;
+        
+        vector<int> v(2,-1);
+        if(nums.size()==1){
+            if(t==nums[lo]) return {0,0};
+            return v;
+        }
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
             if(nums[mid]==t){
-                if(mid!=0&&nums[mid-1]==t){
-                        hi=mid-1;
-                }
-                else{
-                    f=mid;
-                    break;
-                }
-                
+                hi=mid-1;
+                v[0]=mid;
             }
-           if(nums[mid]<t) lo=mid+1;
-           if(nums[mid]>t) hi=mid-1;
+            else if(nums[mid]<t){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
         }
-
- lo=0;
-         hi=nums.size()-1;
+        lo=0;
+        hi=nums.size()-1;
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
-            if(nums[mid]==t){
-                if(mid!=nums.size()-1&&nums[mid+1]==t){
-                        lo=mid+1;
-                }
-                else{
-                    l=mid;
-                    break;
-                }
-                
+             if(nums[mid]==t){
+                lo=mid+1;
+                v[1]=mid;
             }
-           if(nums[mid]<t) lo=mid+1;
-           if(nums[mid]>t) hi=mid-1;
+            else if(nums[mid]<t){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+        
         }
-
-
-        return {f,l};
+        return v;
     }
 };
