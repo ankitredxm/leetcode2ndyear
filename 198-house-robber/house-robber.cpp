5ce2a7d00873d2d448a08649917f6dfa8c032ce7
Ankit Kumar <167794543+ankitredxm@users.noreply.github.com>
2025-12-30 23:04:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (43.93%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,17 @@
 class Solution
 {
     public:
-        int help(vector<int> &nums,int i,vector<int>& v){
-            if(i<0) return 0;
-            if(v[i]!=-1) return v[i];
-            if(i==0) return nums[i];
-            if(i==1) return max(nums[0],nums[1]);
-            return v[i]=max((nums[i] +help(nums,i-2,v)),help(nums,i-1,v));
-        }
+       
 
     int rob(vector<int> &nums) {
-        vector<int> v(nums.size(),-1);
-        return help(nums,nums.size()-1,v);
+        int n=nums.size();
+        vector<int> v(nums.size()+1,-1);
+        v[n]=0;
+        v[n-1]=nums[n-1];
+       for(int i=v.size()-3;i>=0;--i){
+        v[i]=max(nums[i]+v[i+2],v[i+1]);
+       }
+
+       return v[0];
     }
 };
