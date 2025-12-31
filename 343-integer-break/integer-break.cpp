201fc6c2849d28e6f132cb28d9f85e4a3c1591f0
Ankit Kumar <167794543+ankitredxm@users.noreply.github.com>
2025-12-31 20:02:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (52.79%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        // top down
         if(n==1||n==2) return 1;
         if(n==3) return 2;
 
-        vector<int> dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=3;
-        dp[4]=4;
-        int mx=-1;
-        for(int i=5;i<=n;++i){
-            for(int j=1;j<i;++j){
-
-                mx=max(mx,dp[j]*dp[i-j]);
-
-            }
-            dp[i]=mx;
+        int t=n%3;
+        if(t==0) return pow(3,n/3);
+        if(t==2) return pow(3,n/3)*2;
+        else {
+            n=n-4;
+            int p=n/3;
+            return pow(3,p)*4;
         }
 
-        return dp[n];
+        return -1;
     }
 };
