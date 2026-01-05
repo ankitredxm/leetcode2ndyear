2d9452248f9073e61b2b22e82be84df4439d3b3d
Ankit Kumar <167794543+ankitredxm@users.noreply.github.com>
2026-01-05 23:03:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (42.91%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+vector<int> dp;
 int f(int n){
     if(n==1||n==0) return 1;
     // if(n==3) return 5;
     if(n==2) return 2;
     int s=0;
+    if(dp[n]!=-1) return dp[n];
     for(int i=1;i<=n;++i){
         s+=f(abs(1-i))*f(n-i);
     }
 
-    return s;
+    return dp[n]=s;
 }
     int numTrees(int n) {
+        dp.resize(n+1,-1);
         return f(n);
     }
 };
